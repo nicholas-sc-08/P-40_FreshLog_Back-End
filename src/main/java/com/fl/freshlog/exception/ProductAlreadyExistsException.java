b91a29938466af368e77ea867e025ef83a831e1b
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-01-20 10:30:11 -0300

    feat: product services

---
+++ src/main/java/com/fl/freshlog/exception/ProductAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.fl.freshlog.exception;
+
+public class ProductAlreadyExistsException extends RuntimeException {
+    public ProductAlreadyExistsException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/fl/freshlog/service/ProductService.java
+++ src/main/java/com/fl/freshlog/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.fl.freshlog.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -8,7 +9,7 @@ import com.fl.freshlog.dto.CategoryDTO;
 import com.fl.freshlog.dto.ProductDTO;
 import com.fl.freshlog.entity.Category;
 import com.fl.freshlog.entity.Product;
-import com.fl.freshlog.exception.CategoryNotFoundException;
+import com.fl.freshlog.exception.ProductAlreadyExistsException;
 import com.fl.freshlog.exception.ProductNotFoundException;
 import com.fl.freshlog.repository.CategoryRepo;
 import com.fl.freshlog.repository.ProductRepo;
@@ -22,14 +23,25 @@ public class ProductService {
     private final ProductRepo productRepo;
     private final CategoryRepo categoryRepo;
 
+    public List<ProductDTO> getAllProducts() {
+        List<Product> products = productRepo.findAll();
+        return products.stream().map(p -> new ProductDTO(
+            p.getProductId(),
+            p.getName(),
+            p.getCategory().getName(),
+            p.getPrice(),
+            p.getMinStock(),
+            p.getCreatedAt()
+        )).toList();
+    }
+
     public ProductDTO getProductByName(String name) {
         Product product = productRepo.findByName(name).orElseThrow(() -> new ProductNotFoundException("Product with name "+name+" don't exist"));
-        Category category = categoryRepo.findById(product.getCategory().getCategoryId()).orElseThrow(() -> new CategoryNotFoundException("Category with name "+name+" not found"));
 
         ProductDTO productData = new ProductDTO(
             product.getProductId(),
             product.getName(),
-            category.getName(),
+            product.getCategory().getName(),
             product.getPrice(),
             product.getMinStock(),
             product.getCreatedAt()
@@ -38,30 +50,35 @@ public class ProductService {
         return productData;
     }
 
-    public ProductDTO saveProduct(ProductDTO product) {
-        CategoryDTO category = categoryRepo.findByName(product.categoryName());
-
-        Product productData = new Product();
+    public ProductDTO saveProduct(ProductDTO dto) {
+        CategoryDTO category = categoryRepo.findByName(dto.categoryName());
         Category categoryEntity = new Category();
-
+        
         categoryEntity.setCategoryId(category.categoryId());
         categoryEntity.setName(category.name());
 
-        productData.setName(product.name());
+        Product productData = new Product();
+        productRepo.findByName(dto.name()).orElseThrow(() -> new ProductAlreadyExistsException("Product with name "+dto.name()+"already exists!"));
+
+        productData.setName(dto.name());
         productData.setCategory(categoryEntity);
-        productData.setPrice(product.price());
-        productData.setMinStock(product.minStock());
-        productData.setCreatedAt(product.createdAt());
+        productData.setPrice(dto.price());
+        productData.setMinStock(dto.minStock());
+        productData.setCreatedAt(dto.createdAt());
 
         Product savedProduct = productRepo.save(productData);
 
         return new ProductDTO(
             savedProduct.getProductId(), 
             savedProduct.getName(), 
-            product.categoryName(), 
+            savedProduct.getCategory().getName(), 
             savedProduct.getPrice(), 
             savedProduct.getMinStock(),
             savedProduct.getCreatedAt()
         );
+    }
+
+    public void deleteProductById(Integer productId) {
+        productRepo.deleteById(productId);
     }
 }
